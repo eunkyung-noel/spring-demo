d9461d73953cdf9acb3fa0595cd19555e3b7a4e1
eunkyung-noel <dmsruddl0214@gmail.com>
2025-11-19 11:06:59 +0900

    Add CI/CD scripts and specs

--- scripts/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash 
-cd /opt/demo 
-PID=$(pgrep -f 'demo-0.0.1-SNAPSHOT.jar') 
-if [ -n "$PID" ]; then 
-"  echo Stopping old process: $PID" 
-"  kill -9 $PID" 
-fi 
-echo "Starting application..." 
