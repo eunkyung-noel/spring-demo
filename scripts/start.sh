fc8fc785dd1ec8d380b89b279be0fd59ce97fde7
eunkyung-noel <dmsruddl0214@gmail.com>
2025-11-19 10:54:56 +0900

    Add scripts, buildspec and appspec for CI/CD

---
+++ scripts/start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash 
+cd /opt/demo 
+PID=$(pgrep -f 'demo-0.0.1-SNAPSHOT.jar') 
+if [ -n "$PID" ]; then 
+"  echo Stopping old process: $PID" 
+"  kill -9 $PID" 
+fi 
+echo "Starting application..." 
